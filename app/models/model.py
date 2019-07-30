29ce23390f9e50b91d99800b5d09b69b2cc57795
EC2 Default User <ec2-user@ip-172-31-40-148.us-east-2.compute.internal>
2019-07-30 18:13:27 +0000

    first commit

---
+++ app/models/model.py
@@ -0,0 +1,19 @@
+concerts= { "New York":{
+                "Drake": "7/8/19",
+                "Greenday": "8/13/19",
+                "Young Thug": "10/2/19",
+                "Pop Smoke": "10/12/19",
+            },
+            "LA": {
+                "Meek Mill": "9/17/19",
+                "Nipsey Hussle": "10/19/19",
+                "Big Sean": "9/17/19",
+                "J Cole": "10/2/19",
+            },
+            "Paris": {
+                "Post Malone": "8/5/19",
+                "Travis Scott": "9/19/19",
+                "The Weekends": "10/4/19"
+                }
+}           
+        
--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,30 @@ from app.models import model, formopener
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!"
+    return render_template("index.html")
+
+@app.route('/question',methods= ['GET','POST'])
+def question():
+    if request.method == "GET":
+        return "Use the form"
+    else:
+        city = request.form['city']
+        #print (city)
+        date = request.form['date']
+        #print (date)
+        #print (model.concerts)
+        concerts=model.concerts[city]
+        # if(city == 'New York":
+        
+        #     return 
+        # else:
+        return render_template("concerts.html",concerts=concerts)
+        #return ("debugging")
+        
+@app.route('/location')
+def location():
+    return render_template("location.html")
+
+    
+    
+    
